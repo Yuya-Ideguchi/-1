97e42d1aad723cb252dc873d2cc34ab1f7c66837
kapch <kapch@192.168.40.101>
2025-08-30 00:59:34 +0900

    Java導入※必読

---
+++ HelloWorld_Yuya-Ideguchi/src/sample/SelfIntroduction.java
@@ -0,0 +1,24 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+        // 氏名
+		String name = "井手口　裕也";
+		// 年齢
+		int age = 29;
+		// 趣味
+		String hobby = "映像作品鑑賞";
+
+
+		/*
+		* 自己紹介文を出力する処理
+		*/
+
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+	}
+
+}
