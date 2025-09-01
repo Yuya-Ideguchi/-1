0de859e12fb2eaf39ed63eb9806bc3f3854c7b09
kapch <kapch@192.168.40.101>
2025-09-02 00:13:59 +0900

    変数②まで完了

--- HelloWorld_Yuya-Ideguchi/src/curriculamA/CurriculamA.java
+++ HelloWorld_Yuya-Ideguchi/src/curriculamA/CurriculamA.java
@@ -58,6 +58,68 @@ public class CurriculamA {
       System.out.println(doubleNum / shortNum);
       System.out.println(byteNum - shortNum);
       
+ /**************************
+ *       
+ *   【概要】Qes4　nameの宣言→山田太郎を代入→こんにちは、山田太郎さん！とコンソールに表示
+ *   
+ **************************/
+      String name;
+        
+      name ="山田太郎";
+        
+      System.out.println("こんにちは、" + name + "さん！");
+      
+ /**************************
+ *       
+ *   【概要】Qes5　ageの宣言→25を代入→年齢:25歳とコンソールに表示
+ *   
+  **************************/      
+      int age;
+      
+      age = 25;
+      
+      System.out.println("年齢:" + age + "歳");
+      
+/**************************
+ *       
+ *   【概要】Qes6　num1とnum2をそれぞれ宣言し10,5を代入→num1とnum2を足した結果をsumに代入しコンソールに表示
+ *   
+ **************************/       
+      int num1;
+      int num2;
+      
+      num1 = 10;
+      num2 = 5;
+      
+      int sum =(num1 + num2);
+      
+      System.out.println(sum);
+      
+ /**************************
+ *       
+ *   【概要】Qes7　scoreを宣言し80を代入→20を足して更新→最終score:100をscoreを使用して表示
+ *   
+ **************************/       
+      int score;
+      
+      score =80;
+      
+      score = score + 20;
+      
+      System.out.println(score);
+      
+ /**************************
+ *       
+ *   【概要】Qes8　
+ *   
+ **************************/      
+      
+      
+      
+      
+      
+      
+      
       
 	}
 
