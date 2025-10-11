37271c9e2b7a98571354d02e81dbe9dec11a5e54
kapch <kapch@192.168.40.101>
2025-10-12 01:17:55 +0900

    クラス名のスペルミス訂正

--- HelloWorld_Yuya-Ideguchi/src/curriculamB/Question3.java
+++ HelloWorld_Yuya-Ideguchi/src/curriculamB/Question3.java
@@ -3,7 +3,7 @@ package curriculamB;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Qestion3 {
+public class Question3 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
---
+++ HelloWorld_Yuya-Ideguchi/src/object_oriented/Object_oriented.java
@@ -0,0 +1,10 @@
+package object_oriented;
+
+public class Object_oriented {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
