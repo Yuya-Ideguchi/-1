dcece2a084f8b9ef1b02ba972d549c6dc9d49f60
kapch <kapch@192.168.40.101>
2025-11-27 23:05:54 +0900

    ポリモーフィズム修正 MainのgetName取得

--- HelloWorld_Yuya-Ideguchi/src/polymorphism/Main.java
+++ HelloWorld_Yuya-Ideguchi/src/polymorphism/Main.java
@@ -13,10 +13,10 @@ public class Main {
         employees.add(new ContractEmployee("C001", "成歩堂　龍一"));
         employees.add(new ContractEmployee("C002", "夜神　月"));
 
-        // 各社員の給料を出力（ポリモーフィズム）
+        
         for (Employee employee : employees) {
-            int wage = employee.calculateDailyWage(10); // 例：10時間勤務
-            System.out.println(employee.name + " の給料: " + wage + " 円");
+            int wage = employee.calculateDailyWage(10);
+            System.out.println(employee.getName() + " の給料: " + wage + " 円");
         }
     }
 }
